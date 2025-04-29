befe824acf13cce7e9e1de5fd084ac76a3058411
Arman <armaneslami91@gmail.com>
2025-04-29 23:25:28 +0330

    start of the project

---
+++ main.js
@@ -0,0 +1,9 @@
+const toggleTheme = document.getElementById("toggle-theme");
+
+toggleTheme.addEventListener("click", () => {
+  if (document.documentElement.classList.contains("dark")) {
+    document.documentElement.classList.remove("dark");
+  } else {
+    document.documentElement.classList.add("dark");
+  }
+});
